e28e3d1c28f7b4d757beb383d3e0c815390a03ad
zhanghaojie <zhanghaojie@midaigroup.com>
2020-07-02 19:27:41 +0800

    add: Add StepFormClass(class implemention of StepForm) and withStepForm HOC.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,15 @@
 import React from 'react';
-import StepForm, { useStepForm } from './StepForm';
+import StepForm, { useStepForm, StepFormClass, withStepForm } from './StepForm';
 import './App.css';
+
+const Form1 = () => {
+  return (
+    <div>
+      <p>step1</p>
+      <input defaultValue="1" />
+    </div>
+  );
+};
 
 const Form2 = () => {
   const { setStep, length } = useStepForm();
@@ -10,6 +19,35 @@ const Form2 = () => {
       <p>step2</p>
       <input defaultValue="2" />
       can't wait, <button onClick={() => setStep(length - 1)}>finish</button>
+    </div>
+  );
+};
+
+const Thanks = () => {
+  return (
+    <div>
+      <p>thanks.</p>
+    </div>
+  );
+};
+
+const Form5 = withStepForm(({ setStep, length }) => {
+  return (
+    <div>
+      <p>form 5</p>
+      <input defaultValue="5" />
+      can't wait, <button onClick={() => setStep(length - 1)}>finish</button>
+    </div>
+  );
+});
+
+const buttons = ({ hasPrev, prev, hasNext, next, step, length }) => {
+  return (
+    <div key="buttons">
+      <button disabled={!hasPrev} onClick={prev}>Prev</button>
+      <button disabled={!hasNext} onClick={next}>Next</button>
+      <div>---</div>
+      <div>{step + 1}/{length}</div>
     </div>
   );
 };
@@ -28,10 +66,7 @@ function App() {
         }}
 
         {/* form step 1 */}
-        <div>
-          <p>step1</p>
-          <input defaultValue="1" />
-        </div>
+        <Form1/>
 
         {/* form step 2 */}
         <Form2 />
@@ -43,22 +78,19 @@ function App() {
         </div>
 
         {/* form step 4, finished. */}
-        <div>
-          <p>thanks.</p>
-        </div>
+        <Thanks/>
 
         {/* buttons */}
-        {({ hasPrev, prev, hasNext, next, step, length }) => {
-          return (
-            <div key="buttons">
-              <button disabled={!hasPrev} onClick={prev}>Prev</button>
-              <button disabled={!hasNext} onClick={next}>Next</button>
-              <div>---</div>
-              <div>{step + 1}/{length}</div>
-            </div>
-          );
-        }}
+        {buttons}
       </StepForm>
+
+      <StepFormClass>
+        <Form1 />
+        <Form5 />
+        <Form2 />
+        <Thanks />
+        {buttons}
+      </StepFormClass>
     </div>
   );
 }
--- src/StepForm.jsx
+++ src/StepForm.jsx
@@ -50,4 +50,77 @@ export const useStepForm = () => {
   return useContext(StepFormContext);
 };
 
+export class StepFormClass extends React.Component {
+  state = {
+    step: this.props.defaultStep || 0,
+    hasPrev: false,
+    hasNext: true,
+  };
+
+  prev = () => this.setStep(this.state.step - 1);
+
+  next = () => this.setStep(this.state.step + 1);
+
+  getForms = () => this.props.children.filter(React.isValidElement);
+
+  setStep = step => this.setState({ step });
+
+  setHasPrev = hasPrev => this.setState({ hasPrev });
+
+  setHasNext = hasNext => this.setState({ hasNext });
+
+  getContext = () => {
+    const { step, hasPrev, hasNext } = this.state;
+    return {
+      hasPrev,
+      prev: this.prev,
+      hasNext,
+      next: this.next,
+      step,
+      setStep: this.setStep,
+      length: this.getForms().length,
+    };
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    const { step } = this.state;
+    if (prevState.step === step) {
+      return;
+    }
+    this.setHasPrev(step > 0);
+    this.setHasNext(step < this.getForms().length - 1);
+  }
+
+  render() {
+    const context = this.getContext();
+
+    return (
+      <StepFormContext.Provider value={context}>
+        {this.props.children.map((child, index) => {
+          if (React.isValidElement(child)) {
+            const formIndex = this.getForms().indexOf(child);
+            return <div key={child.key || index} style={{ display: formIndex === this.state.step ? 'block' : 'none' }}>{child}</div>
+          }
+          if (typeof child === 'function') {
+            return child(context);
+          }
+          return null;
+        })}
+      </StepFormContext.Provider>
+    );
+  }
+}
+
+export const withStepForm = WrappedComponent => class extends React.Component {
+  static displayName = `withStepForm(${WrappedComponent.displayName || WrappedComponent.name || 'Component'})`;
+
+  render() {
+    return (
+      <StepFormContext.Consumer>
+        {value => <WrappedComponent {...value} {...this.props} />}
+      </StepFormContext.Consumer>
+    );
+  }
+};
+
 export default StepForm;
